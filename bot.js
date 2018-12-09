4684e57ebbffe88a3d9f974eecd22279be78c3b5
YossufAHem <45273413+YossufAHem@users.noreply.github.com>
2018-12-09 04:03:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,18 @@ client.on('ready', () => {
   console.log('bot online ');
 });
 
-
-client.on('message', msg => {
-  if (msg.content === 'هلا ') {
-   msg.reply('**عزيزي الكريم يوسف ليس موجود الحين
+client.on("message", message => {
+	var prefix = "-";
+ if (message.content === "هلا") {
+  const embed = new Discord.RichEmbed()  
+      .setColor("#000000") 
+      .setDescription(`**عزيزي الكريم يوسف ليس موجود الحين
 اتنظر شوي لان يوسف في مشوار 
 ازا مو مصدق اكتب هلا **
-https://cdn.pg.sa/FXUxrKyI4g.gif
-');
-  }
-});
-
-
+https://cdn.pg.sa/FXUxrKyI4g.gif`)
+   message.channel.send(embed)   
+    
+   }
+   }); 
+  
 client.login(process.env.BOT_TOKEN);
