7f5d70ed2e72072b2a21db1e4c6e36f5ebbcd65c
YossufAHem <45273413+YossufAHem@users.noreply.github.com>
2018-12-09 04:00:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,19 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const client1 = new Discord.Client();
-const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-
 client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : YossiF `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`This Bots Online ' `);
-   console.log(`----------------`);
+  console.log('bot online ');
 });
 
+
 client.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
+  if (msg.content === 'هلا ') {
+   msg.reply('**عزيزي الكريم يوسف ليس موجود الحين
+اتنظر شوي لان يوسف في مشوار 
+ازا مو مصدق اكتب هلا **
+https://cdn.pg.sa/FXUxrKyI4g.gif
+');
+  }
 });
 
 
 client.login(process.env.BOT_TOKEN);
-client1.login(process.env.BOT_TOKEN1);
-client2.login(process.env.BOT_TOKEN2);
-client3.login(process.env.BOT_TOKEN3);
