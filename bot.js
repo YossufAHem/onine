76604b1e4dd92b25ee27ed5fe11043ef0f4a1a84
YossufAHem <45273413+YossufAHem@users.noreply.github.com>
2018-12-09 03:48:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,56 +26,7 @@ client.on('message', msg => {
      }
     }
 }
-})
-client1.on('ready', () => {
-    client1.channels.get("497896774967492616").join(); 
-    });
-
-client1.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client1.on('ready', () => {
-    client1.channels.get("497896774967492616").join(); 
-    });
-
-
-client2.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client2.on('ready', () => {
-    client2.channels.get("497896774967492616").join(); 
-    });
-
-client3.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client3.on('ready', () => {
-    client3.channels.get("497896774967492616").join(); 
-    });
+});
 
 
 client.login(process.env.BOT_TOKEN);
